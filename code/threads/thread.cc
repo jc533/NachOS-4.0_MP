a328fca2417ac96852e14f32947cb89447dc8069
JustinChen <yub612@gmail.com>
2024-11-30 22:16:05 +0800

    fix

--- code/threads/thread.cc
+++ code/threads/thread.cc
@@ -34,14 +34,13 @@ const int STACK_FENCEPOST = 0xdedbeef;
 //	"threadName" is an arbitrary string, useful for debugging.
 //----------------------------------------------------------------------
 
-Thread::Thread(char *threadName, int threadID, int pri) {
+Thread::Thread(char *threadName, int threadID) {
     ID = threadID;
     name = threadName;
     isExec = false;
     stackTop = NULL;
     stack = NULL;
     status = JUST_CREATED;
-    priority = pri;
     for (int i = 0; i < MachineStateSize; i++) {
         machineState[i] = NULL;  // not strictly necessary, since
                                  // new thread ignores contents
