bdaf7508128038f416e887e3737a9f6f0f116e7d
JustinChen <yub612@gmail.com>
2024-12-02 04:49:50 +0800

    remove dbg cout

--- code/threads/thread.cc
+++ code/threads/thread.cc
@@ -262,15 +262,11 @@ void Thread::Sleep(bool finishing) {
         kernel->currentThread->updateBurst();
     }
     // cout << "debug Thread::Sleep " << name << "wait for Idle\n";
-    DEBUG(dbgScheduler,"1");
     while ((nextThread = kernel->scheduler->FindNextToRun()) == NULL) {
-        DEBUG(dbgScheduler,"2");
         kernel->interrupt->Idle();  // no one to run, wait for an interrupt
     }
     // returns when it's time for us to run
-    DEBUG(dbgScheduler,"hi");
     kernel->scheduler->Run(nextThread, finishing);
-    DEBUG(dbgScheduler,"dkdkdk");
 }
 
 //----------------------------------------------------------------------
